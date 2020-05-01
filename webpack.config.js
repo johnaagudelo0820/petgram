22f1d792d18bab74de38453241606e682c168a34
john alexander agudelo <johnaagudelo@gmail.com>
2020-05-01 17:21:29 -0500

    add manifest and service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
 
 module.exports = {
   output: {
@@ -22,6 +26,36 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram 🐶',
+      description: 'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [{
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        ios: true
+      }]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://res.cloudinary.com|images.unsplash.com'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-john.johnaagudelo.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ]
 }
